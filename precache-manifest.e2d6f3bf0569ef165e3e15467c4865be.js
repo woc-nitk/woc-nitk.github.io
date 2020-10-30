593b0c7cc37ab4d0674463587c7cb80c41cabacf
Sriram2001 <47755579+Sriram2001@users.noreply.github.com>
2020-10-30 16:14:39 +0000

    Deploying to gh-pages from  @ 08cb6703032c2c475626fe5ff9e395b33da468af 🚀

--- precache-manifest.e2d6f3bf0569ef165e3e15467c4865be.js
+++ precache-manifest.e2d6f3bf0569ef165e3e15467c4865be.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77dbeeb299c1ac4d95cd25364623f98c",
+    "revision": "e60144ad25ebab61c82c56635039a1c7",
     "url": "/index.html"
   },
   {
-    "revision": "76dd4a4cf59ce6d81ff1",
+    "revision": "2006e9c42965f9725d18",
     "url": "/static/css/main.a6944895.chunk.css"
   },
   {
-    "revision": "467c39da4a463ca9e227",
-    "url": "/static/js/2.30f65dd8.chunk.js"
+    "revision": "ed14b0f4edf40929d750",
+    "url": "/static/js/2.e81d1876.chunk.js"
   },
   {
     "revision": "3e41c806c4157d52bd91e7055550c0f6",
-    "url": "/static/js/2.30f65dd8.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e81d1876.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "76dd4a4cf59ce6d81ff1",
-    "url": "/static/js/main.03522cfa.chunk.js"
+    "revision": "2006e9c42965f9725d18",
+    "url": "/static/js/main.74e37984.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84703e237bce08d035be090ad6dc3779.js"
+  "/precache-manifest.e2d6f3bf0569ef165e3e15467c4865be.js"
 );
 
 self.addEventListener('message', (event) => {
