a847eba617914b0462397768b89609ba10642ba1
Sriram2001 <sriramudupi19@gmail.com>
2021-10-26 13:49:45 +0530

    Add mentee guideelines

--- src/components/guidelinespage/Guidelines.js
+++ src/components/guidelinespage/Guidelines.js
@@ -14,7 +14,7 @@ export default function Guidelines() {
           }}
         >
           Guidelines for participating entities
-      </h1>
+        </h1>
 
         <h2>Mentors and participating organizations</h2>
         <p>To participate in Winter of Code NITK 2020, mentors and participating organizations are required to adhere to the following guidelines for the smooth conduction of the program.</p>
@@ -26,6 +26,20 @@ export default function Guidelines() {
           <li>Project proposals have to be submitted at least two days prior to the deadline for them to be reviewed</li>
           <li>The project reviewal committee will be announced after the commencement of the program, and all mentors will have to submit their feedback of the mentees as per the timeline. Mentors will also be evaluated based on the mentee feedback and the decision of continuing the project will be decided by the Winter of Code committee</li>
           <li>In the event that discrepancies arise the decision taken by the Winter of Code team will be final</li>
+        </ul>
+        <br />
+        <h2>Mentees/Students</h2>
+        <p>To participate in NITK Winter of Code 2021, mentees are required to adhere to the following guidelines for the smooth conduction of the program.</p>
+        <ul>
+          <li>Students applying for the program need to fill the application form including a detailed proposal on how they plan to go about the project.</li>
+          <li>The proposal should include all the mandatory fields specified on the respective project page (if any). Further, the student is expected to complete the tasks mentioned on the project page. The status of these tasks can be mentioned in the project proposal.</li>
+          <li>The coding phase of the program is spread across ten weeks that includes a two-week refresh period.</li>
+          <li>The refresh period is a period where no work is expected from the student and can be used to fulfill other commitments. It can be taken anytime during the phase and the details of it should be mentioned in the timeline in the proposal.</li>
+          <li>The program has two evaluations: mid evaluation and final evaluation. The student has to pass both evaluations to successfully complete the program.</li>
+          <li>On successful completion of the program, the student gets a certificate issued by the Center for Open-source Software and Hardware (COSH), NITK.</li>
+          <li>A student can submit applications for up to two projects with his/her preference order. The allotment of the project will make sure a student doesn’t get more than one project, keeping in mind the preference of mentors and the student both.</li>
+          <li>The projects flagged as ‘NITK only’ require the mentee to be a student from NITK. All other projects are open to anyone across the world!</li>
+          <li>In the event that discrepancies arise the decision taken by the Winter of Code team will be final.</li>
         </ul>
       </div>
     </>
