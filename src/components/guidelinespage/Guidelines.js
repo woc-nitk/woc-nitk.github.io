5c171c2916e06025f55ae2cd541918690440a476
Muthu <muthuku37@gmail.com>
2022-09-01 23:34:10 +0530

    Updated Guidelines

--- src/components/guidelinespage/Guidelines.js
+++ src/components/guidelinespage/Guidelines.js
@@ -16,31 +16,48 @@ export default function Guidelines() {
           Guidelines for participating entities
         </h1>
 
-        <h2>Mentors and participating organizations</h2>
-        <p>To participate in Winter of Code NITK 2021, mentors and participating organizations are required to adhere to the following guidelines for the smooth conduction of the program.</p>
-
+        <h2>Participating organizations</h2>
+        <p>To participate in Winter of Code NITK 2021, participating organizations are required to adhere to the following guidelines for the smooth conduction of the program.</p>
+        <h3>Application</h3>
+        <p></p>
         <ul>
-          <li>The mentors mentioned in the project proposal will only be considered responsible for the project and will serve as the point of contact</li>
-          <li>No organization/mentor will be allowed to withdraw from the program at any point in the program duration</li>
-          <li>All mentors and mentoring organizations will be required to follow the timeline proposed by the Winter of Code committee</li>
-          <li>Project proposals have to be submitted at least two days prior to the deadline for them to be reviewed</li>
-          <li>The project reviewal committee will be announced after the commencement of the program, and all mentors will have to submit their feedback of the mentees as per the timeline. Mentors will also be evaluated based on the mentee feedback and the decision of continuing the project will be decided by the Winter of Code committee</li>
-          <li>In the event that discrepancies arise the decision taken by the Winter of Code team will be final</li>
+          <li>Any organization willing to build an open source project or intend to work on an existing project is eligible to apply to <b>NITK Winter of Code</b>. The organization must have the source code of the project made open source that is built during the duration of the program. The previous clause shall not be applicable for organizations mentoring from the <b>National Institute of Technology Karnataka (NITK)</b>.</li>
+          <li>The organization administrator/project mentor must agree to the guidelines applicable for organizations under the <b>NITK Winter of Code</b>. The organization administrator/project mentor must submit the Google form shared by the <b>NITK Winter of Code</b> organizing committee to be considered eligible as a mentoring organization.</li>
+          <li>The <b>Google form for organizations/mentors</b> (a.k.a, Organization Application) must be submitted by an Organization Administrator/project mentor available on the <b>Program Website</b> during the application period for organizations/mentors described in the Program Timeline.</li>
+          <li>The <b>Google form for organizations/mentors</b> must be submitted by individual mentors for each project of an organization/self.</li>
+          <li>The <b>NITK Winter of Code</b> organizing committee reserves the right to decide to accept or reject an organization at its sole discretion.</li>
+          <li>The <b>NITK Winter of Code</b> organizing committee will announce the selected organizations/projects on the <b>Program Website</b>.</li>
         </ul>
-
-        <h2>Mentees/Students</h2>
-        <p>To participate in NITK Winter of Code 2021, mentees are required to adhere to the following guidelines for the smooth conduction of the program.</p>
+        <p></p>
+        <h3>Responsibilities</h3>
+        <p></p>
         <ul>
-          <li>Students applying for the program need to fill the application form including a detailed proposal on how they plan to go about the project.</li>
-          <li>The proposal should include all the mandatory fields specified on the respective project page (if any). Further, the student is expected to complete the tasks mentioned on the project page. The status of these tasks can be mentioned in the project proposal.</li>
-          <li>The coding phase of the program is spread across ten weeks that includes a two-week refresh period.</li>
-          <li>The refresh period is a period where no work is expected from the student and can be used to fulfill other commitments. It can be taken anytime during the phase and the details of it should be mentioned in the timeline in the proposal.</li>
-          <li>The program has two evaluations: mid evaluation and final evaluation. The student has to pass both evaluations to successfully complete the program.</li>
-          <li>On successful completion of the program, the student gets a certificate issued by the Center for Open-source Software and Hardware (COSH), NITK.</li>
-          <li>A student can submit applications for up to two projects with his/her preference order. The allotment of the project will make sure a student doesn’t get more than one project, keeping in mind the preference of mentors and the student both.</li>
-          <li>The projects flagged as ‘NITK only’ require the mentee to be a student from NITK. All other projects are open to anyone across the world!</li>
-          <li>In the event that discrepancies arise the decision taken by the Winter of Code team will be final.</li>
+          <li>In <b>NITK Winter of Code</b>, an organization is considered responsible for appointing mentors for projects and ensuring that mentors fill in the application form for every project independently.</li>
+          <li>An organization shall receive a mention on the <b>Program Website</b> on having at least one project proposal accepted by the <b>NITK Winter of Code</b> organizing committee.</li>
+        </ul>       
+        <h2>Mentors</h2>
+        <p>To participate in <b>NITK Winter of Code</b> 2021, mentors are required to adhere to the following guidelines for the smooth conduction of the program.</p>
+        <h3>Application</h3>
+        <p></p>
+        <ul>
+          <li>All rules & regulations applicable to the mentors of an organization are applicable to any mentor participating in <b>NITK Winter of Code</b>.</li>
+          <li>The application process is common for all mentors, irrespective of their affiliation with any organization. The project mentors need to fill out the organization/mentor form independently for every project they wish to propose and fill in the details of the corresponding mentoring organization (if any).</li>
         </ul>
+        <p></p>
+        <h3>Responsibilities</h3>
+        <p></p>
+        <ul>
+          <li>The mentor(s) mentioned in the project proposal and organization/mentor form for the project will only be considered responsible for the project and will serve as the point of contact.</li>
+          <li>All mentors and mentoring organizations will be required to follow the timeline proposed by the <b>NITK Winter of Code</b> organizing committee.</li>
+          <li>The mentor shall be solely responsible for helping the selected contributor get started in the organization &/or project as part of <b>NITK Winter of Code</b>.</li>
+          <li>The mentor shall be responsible for regularly syncing up with the mentee regarding the project status, work completed, issues, etc.</li>
+          <li>The mentor shall be responsible for guiding and helping the mentee during the entire course of <b>NITK Winter of Code</b>. We consider, similar to Google Summer of Code, that a mentor is the most valuable resource for a mentee during the course of their project.</li>
+          <li>There shall be a project reviewal committee setup, as per the discretion of the <b>NITK Winter of Code</b> organizing committee, after the commencement of the program, and all mentors will have to submit their feedback of the mentees as per the timeline. <b>Adhering to the timeline is a mandate for participation in the program and shall be strictly enforced by the organizing committee.</b></li>
+          <li>Mentors will also be evaluated based on the mentee feedback and the decision of continuing the project will be decided by the <b>NITK Winter of Code</b> organizing committee.</li>
+          <li>In the event that discrepancies arise the decision taken by the <b>NITK Winter of Code</b> organizing committee will be final.</li>
+        </ul>  
+        <br/>
+        <p><i>The decision taken by the organizing committee of Winter of Code shall be final in any case. The organizing committee of Winter of Code reserves the right to suspend, cancel, or modify the structure of the Program. The organizing committee also reserves the right to modify these Program Rules to reflect any modifications to the Program. Any changes made will be effective immediately upon notice which will be notified through the program page and/or email. Continued participation in the Program after such notice will be deemed acceptance of such changes.</i></p> 
       </div>
     </>
   );
