4ccf6b1127a4fb9f3bbd79751a4b53623af2da01
Sushanth Sathesh Rao <57192414+raosush@users.noreply.github.com>
2022-09-20 23:35:13 +0530

    Update incentives, program overview

--- src/components/aboutpage/About.js
+++ src/components/aboutpage/About.js
@@ -24,8 +24,7 @@ export default function About() {
         <h2>Program Overview</h2>
         <p>Winter of Code is a program organized by the Centre for Open-Source Software and Hardware (COSH) at NITK Surathkal with a focus on introducing the student developers across the world to open source software and hardware development. The students can work with projects proposed by the Technical clubs, Open Source Organizations and Faculty Members of NITK over the winter break. This program also plans to introduce to students the rigorous process of developing a project proposal similar to that of GSoC.</p>
         <p>A team of students will act as an organizing committee for the Winter of Code. The organizing committee will ensure that the program is conducted smoothly and will also maintain a website for the event. The organizing committee is responsible for creating a basic filter for project proposals.</p>
-        <p>For the first time this year, in 2021, we have opened the NITK Winter of Code program for the students  other than NITK to submit proposals and participate in this program. They will be mentored by the organizations from NITK Surathkal.</p>
-        <p>Due to the varied schedules of the mentees within and outside NITK, the participants would be allowed to adjust their objectives and avail for a break of utmost two weeks upon discussion with their project mentors. This break is to be clearly mentioned in the timeline of their project proposals. Please note that the timeline of the program will not be altered, the break is to be adjusted by modifying the objectives.</p>
+        <p>For the first time in the year 2021, we have opened the NITK Winter of Code program for the students  other than NITK to submit proposals and participate in this program. They will be mentored by the organizations from NITK Surathkal.</p>
 
         <h2>Target Audience</h2>
         <p>Anyone who is interested in contributing to open source! There will be students from all years and so projects of varying difficulty are needed.</p>
@@ -37,8 +36,9 @@ export default function About() {
 
         <h2>Incentives:</h2>
         <ul>
-          <li>Hands-on experience with open source!</li>
-          <li>Understanding how programs like Google Summer of Code work!</li>
+          <li>Working/building innovating, exciting and positively impacting projects during their winter!<li>
+          <li>Hands-on experience with open source and exposure to collaborativie ways of working!</li>
+          <li>Understanding how global programs like Google Summer of Code work!</li>
           <li>An amazing opportunity to utilize the winter holidays through an impactful project</li>
           <li>Certificate issued by COSH NITK at the end of completion, both for mentors and students (Mentors can decide if students are eligible for certificates)</li>
         </ul>
