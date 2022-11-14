32e8282d7d6853fb4d18bcec13c37a414fa65a0b
raosush <57192414+raosush@users.noreply.github.com>
2022-11-14 11:58:57 +0000

    Deploying to gh-pages from  @ bf4d1a2246bf8fe74d0e996b1b66b4fb3bc67c20 🚀

--- precache-manifest.195c87a53804d316a5c74dbc863968b4.js
+++ precache-manifest.195c87a53804d316a5c74dbc863968b4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "51fafe0cd3365d91b960fb9111ed3825",
+    "revision": "105e3b89d4fab0219143436da1533263",
     "url": "/index.html"
   },
   {
-    "revision": "bf6b1ba3237b303d33c5",
+    "revision": "8e9169894d9727d0ee10",
     "url": "/static/css/main.b23e0c79.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c4155559.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bf6b1ba3237b303d33c5",
-    "url": "/static/js/main.f90b5f2c.chunk.js"
+    "revision": "8e9169894d9727d0ee10",
+    "url": "/static/js/main.0ffac505.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d175f9e3a0d824ce786991fd8aa8ff1.js"
+  "/precache-manifest.195c87a53804d316a5c74dbc863968b4.js"
 );
 
 self.addEventListener('message', (event) => {
