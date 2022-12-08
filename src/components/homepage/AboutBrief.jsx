8dfe58ebf68866c4b2999bbd9bbb142bd8488ff2
nilishalu <shalini.c.e.ganu@gmail.com>
2022-12-08 21:40:18 +0530

    Added about section of the home page

--- src/components/homepage/AboutBrief.jsx
+++ src/components/homepage/AboutBrief.jsx
@@ -2,8 +2,50 @@ import React from 'react'
 
 const AboutBrief = () => {
   return (
-    <section>
-        About
+    <section className='about-brief'>
+        <div className='text'>
+           <h2>
+            What is NITK Winter of Code?
+           </h2>
+           <p>
+            NITK Winter of Code aims at helping students get 
+            started with open source projects that directly
+            impact people's lives. Winter of Code has a philosophy 
+            very similar to larger programs like GSoC and Outreachy.
+            The participants will learn how to prepare a project 
+            application, work in teams, understand the importance 
+            of effective communication, and most importantly, work 
+            with large codebases. They will also learn the intricate
+             workflows and good coding practices.
+           </p>
+           <button className='l-more'>Learn more</button>
+        </div>
+        <div className='numbers'>
+         <div className='info'>
+           <h1>100+</h1>
+           <p>Registrations</p>
+         </div>
+         <div className='info'>
+           <h1>50+</h1>
+           <p>Participants</p>
+         </div>
+         <div className='info'>
+           <h1>10+</h1>
+           <p>Institutes</p>
+         </div>
+         <div className='info'>
+           <h1>5+</h1>
+           <p>Organizations</p>
+         </div>
+         <div className='info'>
+           <h1>20+</h1>
+           <p>Mentors</p>
+         </div>
+         <div className='info'>
+           <h1>3</h1>
+           <p>Years</p>
+         </div>
+        </div>
     </section>
   )
 }
