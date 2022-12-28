5a29df3a4135e42c7be5144048195cff6bb4f88c
nilishalu <shalini.c.e.ganu@gmail.com>
2022-12-28 14:53:12 +0530

    update design of guidelines page

--- src/components/guidelinespage/Guidelines.js
+++ src/components/guidelinespage/Guidelines.js
@@ -1,26 +1,51 @@
 import React from "react";
 
+import Mentee from '../../assets/mentee.png'
+import Mentor from '../../assets/mentor.png'
+import Org from '../../assets/organization.png'
+
+import "./guidelines.css"
 
 
 export default function Guidelines() {
   return (
     <>
-      <div className=" about container">
-        <h1
-          style={{
-            fontSize: "36px",
-            marginTop: "60px",
-            marginBottom: "5px",
-          }}
-        >
-          Guidelines for participating entities
-        </h1>
-
-        <h2>Contributors/Mentees</h2>
-        <p>To participate in Winter of Code NITK 2022, mentees/contributors are required to adhere to the following <a href="https://bit.ly/woc-mentee-guidelines" target="blank">guidelines</a> for the smooth conduction of the program.</p>
-        <h3>Application</h3>
-        <p></p>
-        <ul>
+    <div className="guidelines">
+       <div className="container hero">
+         <h1>Guidelines for the participating entities</h1>
+         <div className="entities">
+         <a href="">
+         <div className="entity">
+          <img src = {Mentee} alt = ""/>
+           <h2>Contributor</h2>
+            <p>Come and contribute to the live projects</p>
+         </div>
+         </a>
+         <a href="">
+         <div className="entity">
+          <img src = {Mentee} alt = ""/>
+           <h2>Contributor</h2>
+            <p>Come and contribute to the live projects</p>
+         </div>
+         </a>
+         <a href="">
+         <div className="entity">
+          <img src = {Mentee} alt = ""/>
+           <h2>Contributor</h2>
+            <p>Come and contribute to the live projects</p>
+         </div>
+         </a>
+         </div>
+       </div>
+       <div className="participants">
+          <h2>Contributors</h2>
+          <p>If you are new to open source, or a person looking to contribute to the live projects, 
+            you are in the right place. Come join us!
+          </p>
+          <div className="entity-info">
+            <div className="info">
+              <h3>Application</h3>
+              <ul>
           <li>Any student, currently enrolled in an accredited institute, willing to contribute to open source is eligible to apply as a contributor to this Program.</li>
           <li>The mentee must agree to the guidelines applicable for mentees/contributors under the <b>NITK Winter of Code</b>. The mentee must submit the Google form shared by the NITK Winter of Code organizing committee to be considered eligible for applying to organizations.</li>
           <li>The <b>Google form for organizations/mentors</b> (a.k.a, Contributor/Mentee Application) must be submitted by the mentee available on the <b>Program Website</b> during the application period for organizations/mentors described in the Program Timeline.</li>
@@ -29,65 +54,37 @@ export default function Guidelines() {
           <li>The <b>mentors/organizations</b> in the <b>NITK Winter of Code</b> reserve the right to decide to accept or reject a mentee at their sole discretion. In case of any grievances, the applicant is instructed to reach out to the organizing committee of <b>NITK Winter of Code</b>.</li>
           <li>The <b>NITK Winter of Code</b> organizing committee will announce the selected mentees on the <b>Program Website</b>.</li>
         </ul>
-        <p></p>
-        <h3>Responsibilities</h3>
-        <p></p>
-        <ul>
+            </div>
+            <div className="info">
+              <h3>Responsibilities</h3>
+              <ul>
           <li>Providing Evaluation of their Mentor(s).</li>
           <li>Work with complete diligence to ensure the project objectives are met(which may be subject to change but only after the consent of the mentor in context).</li>
           <li>Ensure good coding & documentation practices are followed as deemed fit by the organization/mentor.</li>
           <li>Publish a final report documenting the work done during the project(carried out during <b>NITK Winter of Code</b> alone). No work done outside the duration of <b>NITK Winter of Code</b>, will be considered for evaluations or final report submissions and if violated, may lead to disqualification from the program immediately.</li>
           <li>If the organizing committee of <b>NITK Winter of Code</b> reasonably believes that a Contributor has failed to meet the foregoing responsibilities, the organizing committee may remove such Contributor from the Program.</li>
         </ul>
-        <h3>Evaluations</h3>
-        <p></p>
-        <ul>
-          <li>The organizing committee of <b>NITK Winter of Code</b> shall conduct two evaluations during the course of the program, namely - 
-            <ul>
-              <li>
-              Mid Evaluation: On completion of 4 weeks of the program, an evaluation of both mentors and mentees shall be done by obtaining feedback from each entity of the other. It is mandatory that all entities fill out the evaluation form before the stipulated deadline.
-              </li>
-              <li>
-              Final Evaluation: On completion of 8 weeks(end of the program), an evaluation of both mentors and mentees shall be done by obtaining feedback from each entity of the other. As part of this evaluation, the mentee is expected to submit a report documenting the work done as part of the program. It is mandatory that all entities fill out the evaluation form before the stipulated deadline.
-              </li>
-            </ul>
-          </li>
-          <li>Evaluation results are strictly kept confidential and disclosed only to the organizing committee of the <b>NITK Winter of Code</b>. Specific questions are intended to be shared with the other entity and will be specifically called out in the evaluation form.</li>
-          <li>If a mentee/contributor is failed in the mid-evaluation, they will be disqualified with immediate effect from the program. In case of any grievances, the mentees/contributors are instructed to reach out to the organizing committee of <b>NITK Winter of Code</b>.</li>
-          <li>On successful completion(submission of the evaluation form before the deadline), the certificates of each individual shall be generated and shared.</li>
-          <li>Failing to complete evaluations before the stipulated deadline, may lead to revoking of the project and the benefits associated with the completion of the project. No deadline extension shall be granted, except in genuine cases(considered by the organizing committee of <b>NITK Winter of Code</b>).</li>
-        </ul>
-        <h2>Participating organizations</h2>
-        <p>To participate in Winter of Code NITK 2022, participating organizations are required to adhere to the following <a href="https://bit.ly/woc-mentor-guidelines" target="blank">guidelines</a> for the smooth conduction of the program.</p>
-        <h3>Application</h3>
-        <p></p>
-        <ul>
-          <li>Any organization willing to build an open source project or intend to work on an existing project is eligible to apply to <b>NITK Winter of Code</b>. The organization must have the source code of the project made open source that is built during the duration of the program. The previous clause shall not be applicable for organizations mentoring from the <b>National Institute of Technology Karnataka (NITK)</b>.</li>
-          <li>The organization administrator/project mentor must agree to the guidelines applicable for organizations under the <b>NITK Winter of Code</b>. The organization administrator/project mentor must submit the Google form shared by the <b>NITK Winter of Code</b> organizing committee to be considered eligible as a mentoring organization.</li>
-          <li>The <b>Google form for organizations/mentors</b> (a.k.a, Organization Application) must be submitted by an Organization Administrator/project mentor available on the <b>Program Website</b> during the application period for organizations/mentors described in the Program Timeline.</li>
-          <li>The <b>Google form for organizations/mentors</b> must be submitted by individual mentors for each project of an organization/self.</li>
-          <li>The <b>NITK Winter of Code</b> organizing committee reserves the right to decide to accept or reject an organization at its sole discretion.</li>
-          <li>The <b>NITK Winter of Code</b> organizing committee will announce the selected organizations/projects on the <b>Program Website</b>.</li>
-        </ul>
-        <p></p>
-        <h3>Responsibilities</h3>
-        <p></p>
-        <ul>
-          <li>In <b>NITK Winter of Code</b>, an organization is considered responsible for appointing mentors for projects and ensuring that mentors fill in the application form for every project independently.</li>
-          <li>An organization shall receive a mention on the <b>Program Website</b> on having at least one project proposal accepted by the <b>NITK Winter of Code</b> organizing committee.</li>
-        </ul>       
-        <h2>Mentors</h2>
-        <p>To participate in <b>NITK Winter of Code</b> 2022, mentors are required to adhere to the following <a href="https://bit.ly/woc-mentor-guidelines" target="blank">guidelines</a> for the smooth conduction of the program.</p>
-        <h3>Application</h3>
-        <p></p>
-        <ul>
+            </div>
+          </div>
+          <a href="https://bit.ly/woc-mentee-guidelines" target="blank"><button className="read">Read more</button></a>
+       </div>
+
+       <div className="participants mentor">
+          <h2>Mentors</h2>
+          <p>If you are new to open source, or a person looking to contribute to the live projects, 
+            you are in the right place. Come join us!
+          </p>
+          <div className="entity-info">
+            <div className="info">
+              <h3>Application</h3>
+              <ul>
           <li>All rules & regulations applicable to the mentors of an organization are applicable to any mentor participating in <b>NITK Winter of Code</b>.</li>
           <li>The application process is common for all mentors, irrespective of their affiliation with any organization. The project mentors need to fill out the organization/mentor form independently for every project they wish to propose and fill in the details of the corresponding mentoring organization (if any).</li>
         </ul>
-        <p></p>
-        <h3>Responsibilities</h3>
-        <p></p>
-        <ul>
+            </div>
+            <div className="info">
+              <h3>Responsibilities</h3>
+              <ul>
           <li>The mentor(s) mentioned in the project proposal and organization/mentor form for the project will only be considered responsible for the project and will serve as the point of contact.</li>
           <li>All mentors and mentoring organizations will be required to follow the timeline proposed by the <b>NITK Winter of Code</b> organizing committee.</li>
           <li>The mentor shall be solely responsible for helping the selected contributor get started in the organization &/or project as part of <b>NITK Winter of Code</b>.</li>
@@ -97,9 +94,41 @@ export default function Guidelines() {
           <li>Mentors will also be evaluated based on the mentee feedback and the decision of continuing the project will be decided by the <b>NITK Winter of Code</b> organizing committee.</li>
           <li>In the event that discrepancies arise the decision taken by the <b>NITK Winter of Code</b> organizing committee will be final.</li>
         </ul>  
-        <br/>
-        <p><i>The decision taken by the organizing committee of Winter of Code shall be final in any case. The organizing committee of Winter of Code reserves the right to suspend, cancel, or modify the structure of the Program. The organizing committee also reserves the right to modify these Program Rules to reflect any modifications to the Program. Any changes made will be effective immediately upon notice which will be notified through the program page and/or email. Continued participation in the Program after such notice will be deemed acceptance of such changes.</i></p> 
-      </div>
+            </div>
+          </div>
+          <a href="https://bit.ly/woc-mentee-guidelines" target="blank"><button className="read">Read more</button></a>
+       </div>
+
+       <div className="participants">
+          <h2>Contributors</h2>
+          <p>If you are new to open source, or a person looking to contribute to the live projects, 
+            you are in the right place. Come join us!
+          </p>
+          <div className="entity-info">
+            <div className="info">
+              <h3>Application</h3>
+              <ul>
+          <li>Any organization willing to build an open source project or intend to work on an existing project is eligible to apply to <b>NITK Winter of Code</b>. The organization must have the source code of the project made open source that is built during the duration of the program. The previous clause shall not be applicable for organizations mentoring from the <b>National Institute of Technology Karnataka (NITK)</b>.</li>
+          <li>The organization administrator/project mentor must agree to the guidelines applicable for organizations under the <b>NITK Winter of Code</b>. The organization administrator/project mentor must submit the Google form shared by the <b>NITK Winter of Code</b> organizing committee to be considered eligible as a mentoring organization.</li>
+          <li>The <b>Google form for organizations/mentors</b> (a.k.a, Organization Application) must be submitted by an Organization Administrator/project mentor available on the <b>Program Website</b> during the application period for organizations/mentors described in the Program Timeline.</li>
+          <li>The <b>Google form for organizations/mentors</b> must be submitted by individual mentors for each project of an organization/self.</li>
+          <li>The <b>NITK Winter of Code</b> organizing committee reserves the right to decide to accept or reject an organization at its sole discretion.</li>
+          <li>The <b>NITK Winter of Code</b> organizing committee will announce the selected organizations/projects on the <b>Program Website</b>.</li>
+        </ul>
+            </div>
+            <div className="info">
+              <h3>Responsibilities</h3>
+              <ul>
+          <li>In <b>NITK Winter of Code</b>, an organization is considered responsible for appointing mentors for projects and ensuring that mentors fill in the application form for every project independently.</li>
+          <li>An organization shall receive a mention on the <b>Program Website</b> on having at least one project proposal accepted by the <b>NITK Winter of Code</b> organizing committee.</li>
+        </ul>
+            </div>
+          </div>
+          <a href="https://bit.ly/woc-mentee-guidelines" target="blank"><button className="read">Read more</button></a>
+       </div>
+
+       
+    </div>
     </>
   );
 }
